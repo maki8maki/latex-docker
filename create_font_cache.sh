b7e5cffd125ac17da53a363f2eb92e9cb155c256
Being24 <34680324+being24@users.noreply.github.com>
2023-10-24 21:27:10 +0900

    Update create_font_cache.sh

--- create_font_cache.sh
+++ create_font_cache.sh
@@ -1,3 +1,3 @@
-for x in $(luaotfload-tool --list=* --fields=fullpath); do \
-    /bin/echo "\\relax\\input luaotfload.sty \\font\\x[$x]\\bye" | luatex > /dev/null; \
+for x in $(luaotfload-tool --list=* --fields=fullpath); do
+    luatex -interaction=nonstopmode "\\relax\\input luaotfload.sty \\font\\x[$x]\\bye" > /dev/null
 done
